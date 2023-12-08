ba1755683daaeda1c27e1c52948f5b90be3e24ea
Shao <leo21604212@gmail.com>
2023-12-08 13:09:58 -0500

    qeqwe

---
+++ vite-project/js/oh.js
@@ -0,0 +1,10 @@
+
+async function getData() {
+    let res = await fetch(
+        "https://api.nookipedia.com/villagers?&api_key=cdaec8de-8006-4747-9dec-088bf32c815c"
+    );
+    let data = await res.json();
+
+    data.forEach((villagers) => console.log(villagers.name));
+}
+getData();
