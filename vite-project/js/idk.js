9ad14342183b1b6ff2941dfe91246bf4150b53cf
Shao <leo21604212@gmail.com>
2023-12-13 13:11:13 -0500

    3312

---
+++ vite-project/js/idk.js
@@ -0,0 +1,6 @@
+const DOMselectors = {
+    Thing: document.querySelector(".Thing"),
+    gallery: document.querySelector(".gallery"),
+    card: document.querySelectorAll(".gall"),
+}
+export {DOMselectors}
--- vite-project/js/oh.js
+++ vite-project/js/oh.js
@@ -1,10 +1,23 @@
+import { DOMselectors } from "./idk.js";
 
 async function getData() {
     let res = await fetch(
         "https://api.nookipedia.com/villagers?&api_key=cdaec8de-8006-4747-9dec-088bf32c815c"
     );
     let villagers = await res.json();
-
-    villagers.forEach((villager) => console.log(villager.name, villager.species, villager.phrase));
+    /* villagers.forEach((villager) => console.log(villager.name)); */
+    villagers.forEach((villager) =>
+        DOMselectors.gallery.insertAdjacentHTML(
+            "beforeend",
+            `<div class="gall">
+                   <h2 class="Name">${villager.name}</h2>
+                   <img src="${villager.image_url}" alt="Villager Image" class="img"/>
+                   <h3 class="Species">${villager.species}</h3>
+                   <h3 class="StarSign">${villager.sign}</h3>
+               </div>`
+        )
+    );
 }
 getData();
+
+
