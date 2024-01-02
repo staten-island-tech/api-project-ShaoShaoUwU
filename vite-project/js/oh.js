e49269be5a20b684f8f56fda4ebf16d7d1fed4c7
Shao <leo21604212@gmail.com>
2024-01-02 13:13:18 -0500

    erewq

--- vite-project/js/oh.js
+++ vite-project/js/oh.js
@@ -1,12 +1,12 @@
 import { DOMselectors } from "./idk";
-const value = DOMselectors.Thing.value
+/*const value = DOMselectors.Thing.value
 let API = `https://api.nookipedia.com/villagers?name=${value}&api_key=cdaec8de-8006-4747-9dec-088bf32c815c`
 async function getData(API) {
     let res = await fetch(API)
     let villagers = await res.json()
     function POOOP() {
         DOMselectors.Thing.addEventListener("click", function (event) {
-            /* villagers.forEach((villager) => console.log(villager.name)); */
+             villagers.forEach((villager) => console.log(villager.name)); 
             villagers.forEach((villager) =>
                 DOMselectors.gallery.insertAdjacentHTML(
                     "beforeend",
@@ -21,6 +21,26 @@ async function getData(API) {
         })
     }
     POOOP();
-}
-getData(API);
+} 
+getData(API); */
+async function getData() {
+        /* villagers.forEach((villager) => console.log(villager.name)); */
+        let res = await fetch("https://api.nookipedia.com/villagers?&api_key=cdaec8de-8006-4747-9dec-088bf32c815c");
+        let villagers = await res.json();
+        villagers.forEach((villager) =>
+            DOMselectors.gallery.insertAdjacentHTML(
+                "beforeend",
+                `<div class="gall">
+                   <h2 class="Name">${villager.name}</h2>
+                   <img src="${villager.image_url}" alt="Villager Image" class="img"/>
+                   <h3 class="Species">${villager.species}</h3>
+                   <h3 class="StarSign">${villager.sign}</h3>
+               </div>`
+            )
+        );
+    }
+
+
+
+getData();
 
