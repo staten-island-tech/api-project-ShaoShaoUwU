bf05c7d48cfe76b81c85a4a30f2283cf4afd2694
Shao <leo21604212@gmail.com>
2024-01-03 13:12:36 -0500

    4432

--- vite-project/js/oh.js
+++ vite-project/js/oh.js
@@ -1,30 +1,40 @@
 import { DOMselectors } from "./idk";
-/*const value = DOMselectors.Thing.value
-let API = `https://api.nookipedia.com/villagers?name=${value}&api_key=cdaec8de-8006-4747-9dec-088bf32c815c`
-async function getData(API) {
-    let res = await fetch(API)
-    let villagers = await res.json()
-    function POOOP() {
-        DOMselectors.Thing.addEventListener("click", function (event) {
-             villagers.forEach((villager) => console.log(villager.name)); 
-            villagers.forEach((villager) =>
-                DOMselectors.gallery.insertAdjacentHTML(
-                    "beforeend",
-                    `<div class="gall">
+
+function roop {
+    DOMselectors.Thing.addEventListener("click", function () {
+        async function getData() {
+            let value = DOMselectors.Thing.value
+            let API = `https://api.nookipedia.com/villagers?name=${value}&api_key=cdaec8de-8006-4747-9dec-088bf32c815c`
+            try {
+                let res = await fetch(API);
+                if (!response.good) {
+                    throw new Error('slut you have an error');
+                }
+                let villagers = await res.json();
+                POOOP(villagers);
+            } catch (error) {
+                console.error('zzz', error);
+            }
+            function POOOP() {
+                villagers.forEach((villager) =>
+                    DOMselectors.gallery.insertAdjacentHTML(
+                        "beforeend",
+                        `<div class="gall">
                    <h2 class="Name">${villager.name}</h2>
                    <img src="${villager.image_url}" alt="Villager Image" class="img"/>
                    <h3 class="Species">${villager.species}</h3>
                    <h3 class="StarSign">${villager.sign}</h3>
                </div>`
-                )
-            );
-        })
-    }
-    POOOP();
-} 
-getData(API); */
-async function getData() {
-        /* villagers.forEach((villager) => console.log(villager.name)); */
+                    )
+                );
+            }
+    )}
+};
+
+roop();
+getData();
+/* async function getData() {
+        /* villagers.forEach((villager) => console.log(villager.name)); 
         let res = await fetch("https://api.nookipedia.com/villagers?&api_key=cdaec8de-8006-4747-9dec-088bf32c815c");
         let villagers = await res.json();
         villagers.forEach((villager) =>
@@ -43,4 +53,4 @@ async function getData() {
 
 
 getData();
-
+ */
