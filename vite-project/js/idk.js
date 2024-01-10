ab6f00be76573d5511e292f1d36c0acf5e2f7385
Shao <leo21604212@gmail.com>
2024-01-10 13:08:43 -0500

    213123123

--- vite-project/js/idk.js
+++ vite-project/js/idk.js
@@ -1,6 +1,7 @@
 export const DOMselectors = {
     search: document.querySelector(".search"),
     gallery: document.querySelector(".gallery"),
+    galleri: document.querySelector(".galleri"),
     card: document.querySelectorAll(".gall"),
     enter: document.querySelector("#enter")
 }
--- vite-project/js/oh.js
+++ vite-project/js/oh.js
@@ -1,5 +1,6 @@
 import { DOMselectors } from "./idk";
 const API = `https://api.nookipedia.com/villagers?&api_key=cdaec8de-8006-4747-9dec-088bf32c815c`
+const Art = 'https://api.nookipedia.com/nh/art?&api_key=cdaec8de-8006-4747-9dec-088bf32c815c'
 async function getData(API) {
     try {
         const res = await fetch(API);
@@ -44,6 +45,50 @@ async function getData(API) {
     }
 }
 getData(API);
+async function getArt(Art) {
+    try {
+        const res = await fetch(Art);
+        console.log(res);
+        if (res.status != 200) {
+            throw new Error(res.statusText);
+        }
+
+        const arts = await res.json();
+
+        console.log(arts);
+
+
+        function ello(arr) {
+            arr.forEach((art) =>
+                DOMselectors.galleri.insertAdjacentHTML(
+                    "beforeend",
+                    `<div class="gall">
+           <h2 class="Name">${art.name}</h2>
+           <img src="${art.real_info.image_url}" alt="Villager Image" class="img"/>
+           <h3 class="Species">${art.author}</h3>
+           <h3 class="StarSign">${art.year}</h3>
+       </div>` )
+            );
+
+        }
+        ello(arts);
+
+        function pello(arr) {
+         DOMselectors.enter.addEventListener("click", function (event) {
+            event.preventDefault();
+            DOMselectors.gallery.innerHTML = ""
+            let aname = DOMselectors.search.value
+            let found = arr.filter((art) => art.name.includes((`${aname}`)))
+               ello(found);    
+            }
+         )
+     }
+        pello(arts);
+    } catch (error) {
+        console.log(error)
+    }
+}
+getArt(Art);
 
 /* function roop {
     DOMselectors.Thing.addEventListener("click", function () {
