7589f8863dcb3650da280605194d694c5c173dde
Shao <leo21604212@gmail.com>
2024-01-11 12:50:32 -0500

    weqq

--- vite-project/js/idk.js
+++ vite-project/js/idk.js
@@ -1,7 +1,9 @@
 export const DOMselectors = {
     search: document.querySelector(".search"),
+    artsearch: document.querySelector(".artsearch"),
     gallery: document.querySelector(".gallery"),
     galleri: document.querySelector(".galleri"),
     card: document.querySelectorAll(".gall"),
-    enter: document.querySelector("#enter")
+    enter: document.querySelector("#enter"),
+    artenter: document.querySelector("#artenter")
 }
